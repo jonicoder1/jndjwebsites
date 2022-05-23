2e6b8cc260ed4c25a13e5b465cb32cb7a8977a9d
jonisland <jogucareers@gmail.com>
2022-05-23 10:54:07 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import AboutMe from "./components/about-me";
+
+function App() {
+  return (
+    <div className="App">
+      <AboutMe />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/about-me.js
@@ -0,0 +1,5 @@
+const AboutMe = () => {
+  return console.log("this is About Me section");
+};
+
+export default AboutMe;
