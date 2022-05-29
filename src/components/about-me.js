44ef1a3770a983477849008c0a2f533e4be85ef1
jonisland <jogucareers@gmail.com>
2022-05-28 20:39:39 -0700

    added about section

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -1,9 +1,29 @@
 import classes from "./about-me.module.css";
+import smallCircle from "../images/small_circle.png";
 
 const AboutMe = () => {
   return (
-    <div className={classes}>
-      <h1>About Me</h1>
+    <div className={classes.container}>
+      <h1>About Section</h1>
+      <br />
+      <br />
+      <div className={classes.textContainer}>
+        <img src={smallCircle} alt="" className={classes.smallCircle} />
+        <p>
+          Welcome to jndjwebsites, <br />
+          we build websites for individuals, businesses, <br />
+          companies &/or people who just want to showcase their work.
+          <br /> <br />
+          Our goal is to give you the best experience while also giving you a
+          great
+          <br />
+          website for your business.
+          <br />
+          <br />
+          Our customer service is top notch and we are dedicated to <br />
+          giving you the best service
+        </p>
+      </div>
       <br />
     </div>
   );
